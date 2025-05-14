c3f7f24507e2edabfb1c4f54b2d8485195f30396
Vinicius Souza <vini120@hotmail.com>
2025-05-14 10:55:34 -0300

    FEAT: Adição das rotas de atualização e remoção de cardápios

--- src/Controllers/foodController.js
+++ src/Controllers/foodController.js
@@ -1,18 +1,23 @@
 import foodModel from "../models/foodModel.js";
 
-const create = async (req, res) =>{
-try {
-    const {nome, diaid, tipo_ref} = req.body
-    const insertMenu = await foodModel.create({
-        nome, diaid, tipo_ref
-    })
+const create = async (req, res) => {
+    try {
+        const { insertValues } = req.body
 
-    return res.status(201).json(insertMenu)
-} catch (error) {
-    return res.status(500).json(error.message)
+        for (const value of insertValues) {
+            await foodModel.create({
+                'nome': value.nome,
+                'diaid': value.diaid,
+                'tipo_ref': value.tipo_ref
+            })
+        }
+
+        return res.status(201).json(await foodModel.get())
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
 }
-}
-const get = async(req, res)=>{
+const get = async (req, res) => {
     try {
         const searchMenu = await foodModel.get()
 
@@ -21,7 +26,36 @@ const get = async(req, res)=>{
         return res.status(501).json(error.message)
     }
 }
+const update = async (req, res) => {
+    try {
+        const { updateValues } = req.body
+        for (const value of updateValues) {
+            await foodModel.update({
+                'diaid': value.diaid,
+                'tipo_ref': value.tipo_ref
+            }, {
+                'nome': value.nome
+            })
+
+        }
+        return res.status(200).json(await foodModel.get())
+    } catch (error) {
+        return error.message
+    }
+}
+const remove = async (req, res) => {
+    try {
+        const { days } = req.body
+        for (const dayName of days) {
+            foodModel.remove(dayName)
+        }
+
+        return res.status(200).json(await foodModel.get())
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
 
 export default {
-    create, get
+    create, get, update, remove
 }
